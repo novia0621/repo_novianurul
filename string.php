b6c8588c4adf1519de9a86f0898d213ed6736f9f
novia0621 <141377711+novia0621@users.noreply.github.com>
2023-08-04 08:12:09 +0700

    Add files via upload

---
+++ string.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstname ='novia';
+$lastname ='nurul';
+$street ="jl.katapang";
+$city ="bandung";
+
+$fullname = $firstname. $lastname.'<br>';
+$address = "{$street}, {$city} <br>";
+
+echo $fullname;
+echo $address;
+
+?>
